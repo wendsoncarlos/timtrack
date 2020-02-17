6701e5276656d37e7f01f7278b28fb84ae825687
wendsoncarlos <wendsonsilva@eng.ci.ufpb.br>
2020-02-17 19:59:31 -0300

    Updates

---
+++ precache-manifest.b9b433a88e39ba37664c84929d8831d6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd8cf5b245541292fdc71040d09c95db",
+    "url": "/timtrack/index.html"
+  },
+  {
+    "revision": "7df7b70ed27422bb1446",
+    "url": "/timtrack/static/css/main.6895c390.chunk.css"
+  },
+  {
+    "revision": "93f6d75bf8a643194c32",
+    "url": "/timtrack/static/js/2.977c832e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/timtrack/static/js/2.977c832e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7df7b70ed27422bb1446",
+    "url": "/timtrack/static/js/main.710f9954.chunk.js"
+  },
+  {
+    "revision": "3d2f624b4d5b607f339e",
+    "url": "/timtrack/static/js/runtime-main.2f8c0862.js"
+  },
+  {
+    "revision": "79026f0bc3aa577e45304a0032e22254",
+    "url": "/timtrack/static/media/animal.79026f0b.png"
+  },
+  {
+    "revision": "cb76117aa83093f90a2e56b4f30fefb3",
+    "url": "/timtrack/static/media/peixe.cb76117a.png"
+  },
+  {
+    "revision": "80b6353860c44c9091f0a0a59fb6a1a7",
+    "url": "/timtrack/static/media/texto1.80b63538.png"
+  },
+  {
+    "revision": "875c951c3340128fa542f4954bc8fe06",
+    "url": "/timtrack/static/media/texto2.875c951c.png"
+  },
+  {
+    "revision": "bdf457cc21750c3902c6777e7609be31",
+    "url": "/timtrack/static/media/texto3.bdf457cc.png"
+  },
+  {
+    "revision": "bb7a8dd1ac2990a71ee88a3175c6c3b9",
+    "url": "/timtrack/static/media/texto4.bb7a8dd1.png"
+  }
+]);
